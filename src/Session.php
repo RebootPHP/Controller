2d8edff32e6a9e57eee7e8fb2068cffb51982622
Jan Kristian Valgijainen <54046100+ogchibi95@users.noreply.github.com>
2024-11-01 06:12:47 +0200

    Update Session.php

--- src/Session.php
+++ src/Session.php
@@ -1,2 +1,21 @@
 <?php
 
+class Session {
+
+public function __construct() {
+  session_start();
+
+  if($_SESSION['user_id'] == session_id()) {
+
+  session_regenerate_id();
+  } 
+
+  if (isset($_SESSION['start']) && (time() - $_SESSION['start'] > 120)) {
+    $this->destroy();
+} 
+
+public function destroy() {
+  session_unset();
+
+  session_destroy();
+} } 
